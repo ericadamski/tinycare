7760b1ca919ce8bb7285e1f81a6a7cc429354311
Eric Adamski <er.adamski@gmail.com>
2018-03-18 21:08:27 -0400

    Should be good, needs some live testing

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import uglify from "rollup-plugin-uglify";
+import json from "rollup-plugin-json";
 
 export default {
   input: "src/index.js",
@@ -11,22 +12,18 @@ export default {
     sourcemap: true
   },
   plugins: [
-    resolve({
-      customResolveOptions: {
-        moduleDirectory: "node_modules"
-      }
-    }),
+    resolve(),
     babel({
+      babelrc: false,
       exclude: "node_modules/**",
+      presets: [["env", { modules: false }], "flow"],
       plugins: ["external-helpers"]
     }),
-    commonjs({
-      namedExports: {
-        rxjs: ["Observable"]
-      }
-    }),
+    commonjs(),
+    json(),
     uglify({
       sourceMap: true
     })
-  ]
+  ],
+  external: ["twit"]
 };
--- src/tinycare.js
+++ src/tinycare.js
@@ -1,37 +1,93 @@
 /* @flow */
+import Twit from "twit";
 import { Subject } from "rxjs/Subject";
+import { Observable } from "rxjs/Observable";
 import { Subscription } from "rxjs/Subscription";
-import { filter } from "rxjs/operators";
+import { filter, takeUntil, take } from "rxjs/operators";
 import "rxjs/add/observable/interval";
 
-const TwitterKeys = ["apiKey", "apiSecret", "consumerKey", "consumerSecret"];
+const TwitterKeys = [
+  "accessKey",
+  "accessSecret",
+  "consumerKey",
+  "consumerSecret"
+];
 
 type TwitterConfig = {
-  apiKey: string,
-  apiSecret: string,
+  accessToken: string,
+  accessSecret: string,
   consumerKey: string,
   consumerSecret: string
 };
 
 let _subscriptions;
-export let _canStartTimer$;
+let _canStartTimer$;
+let _breakTaken$;
+let _tweet$;
+let _t;
 
-export function emitCanStartTimer(value: boolean): void {
-  if (!_canStartTimer$)
-    throw new Error("Please make sure you call tinycare(options) first.");
+function initFirst(source$, fn) {
+  return function() {
+    if (!source$)
+      throw new Error("Please make sure you call tinycare(options) first.");
 
-  _canStartTimer$.next(value);
+    return fn(...arguments);
+  };
 }
 
-export function timer(time: number): () => Observable<number> {
-  return () => Observable.interval(time);
+export const emitCanStartTimer = initFirst(_canStartTimer$, v =>
+  _canStartTimer$.next(v)
+);
+
+export const emitBreakTaken = initFirst(_breakTaken$, v =>
+  _breakTaken$.next(v)
+);
+
+function getTweet(): void {
+  const opts = {
+    exclude_replies: true,
+    include_rts: false,
+    count: 1,
+    screen_name: "tinycarebot"
+  };
+
+  _t.get(
+    "statuses/user_timeline",
+    opts,
+    (err, [{ text, user }]) =>
+      err
+        ? _tweet$.error(
+            new Error(
+              "This didn't work. Maybe you didn't set up the twitter API keys?"
+            )
+          )
+        : _tweet$.next({ text, bot: user.screen_name })
+  );
+}
+
+function fetchFromTwitter(source$: Observable<number>): Observable<number> {
+  addSubscription(source$.subscribe(getTweet));
+
+  return source$;
+}
+
+function timer(time: number): () => Observable<number> {
+  const break$ = _breakTaken$.pipe(filter(v => v));
+
+  return () => (
+    addSubscription(break$.subscribe(startTimer(time))),
+    fetchFromTwitter(Observable.interval(time).pipe(takeUntil(break$)))
+  );
 }
 
 function startTimer(time: number): void {
+  unsubscribe();
+  !_canStartTimer$ && (_canStartTimer$ = new Subject());
+
   addSubscription(
     _canStartTimer$
       .pipe(filter(v => v))
-      .take(1)
+      .pipe(take(1))
       .subscribe(timer(time))
   );
 }
@@ -46,7 +102,8 @@ function unsubscribe(): void {
 
 export default function tinycare(options: {
   twitter: TwitterConfig,
-  breakTime: number
+  breakTime: number,
+  onCareNotification: func
 }): void {
   let twitterKeys;
 
@@ -60,5 +117,19 @@ export default function tinycare(options: {
   )
     throw new Error("Missing or incorrect Twitter config.");
 
-  !_canStartTimer$ && (_canStartTimer$ = new Subject());
+  !_breakTaken$ && (_breakTaken$ = new Subject());
+  !_tweet$ && (_tweet$ = new Subject());
+
+  process.env.NODE_ENV !== "test" &&
+    !_t &&
+    (_t = new Twit({
+      consumer_key: options.twitter.consumerKey,
+      consumer_secret: options.twitter.consumerSecret,
+      access_token: options.twitter.accessToken,
+      access_token_secret: options.twitter.accessSecret
+    }));
+
+  startTimer(options.breakTime || 50 * (60 * 1000));
+
+  _tweet$.subscribe(options.onCareNotification);
 }
